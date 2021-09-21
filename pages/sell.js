9c09d92ea3b2a59a620d80d2adc46cbedbb64996
Luis Pineda <lspnd00@gmail.com>
2021-09-20 22:31:07 -0400

    html weird setup in next js, proptypes

---
+++ pages/sell.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Page from '../components/Page'
+
+function Sell() {
+  return (
+    <>
+      <h1>I am the sell page</h1>
+      <p>This item is for sale</p>
+    </>
+  )
+}
+
+export default Sell
