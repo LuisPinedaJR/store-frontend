26d91cb575e0a4cd85aff423cc0dc7cb29409357
Luis Pineda <lspnd00@gmail.com>
2021-09-22 00:25:48 -0400

    styled header using styled components

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,9 @@
-import Document, { Html, Head, NextScript, Main } from 'next/document'
+import Document, { Html, NextScript, Main } from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-US">
-        {/* <Head></Head> */}
         <body>
           <Main />
           <NextScript />
